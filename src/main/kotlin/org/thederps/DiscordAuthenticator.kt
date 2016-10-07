85ec834c4b04a5cbe2fd92b3d58c3cbf58f282f6
Vidmantas Kerbelis <vkerbelis@yahoo.com>
2016-10-07 23:28:57 +0300

    Log bot ready info on clients onReady event

--- src/main/kotlin/org/thederps/DiscordAuthenticator.kt
+++ src/main/kotlin/org/thederps/DiscordAuthenticator.kt
@@ -1,14 +1,29 @@
 package org.thederps
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.thederps.client.ClientRetriever
+import sx.blah.discord.api.events.EventSubscriber
+import sx.blah.discord.handle.impl.events.ReadyEvent
 
 /**
  * @author Vidmantas on 2016-10-07.
  */
 class DiscordAuthenticator(clientRetriever: ClientRetriever) : Authenticator {
+    private val log: Logger
     private val client = clientRetriever.getClient()
+
+    init {
+        log = LoggerFactory.getLogger(DiscordAuthenticator::class.java)
+    }
 
     override fun login() {
         client.login()
+        client.dispatcher.registerListener(this)
+    }
+
+    @EventSubscriber
+    fun onReady(event: ReadyEvent) {
+        log.info("*** The Bot is ready")
     }
 }
--- src/test/kotlin/org/thederps/DiscordAuthenticatorTest.kt
+++ src/test/kotlin/org/thederps/DiscordAuthenticatorTest.kt
@@ -1,23 +1,48 @@
 package org.thederps
 
+import org.junit.Before
 import org.junit.Test
 import org.mockito.Mockito.*
 import org.thederps.client.ClientRetriever
 import sx.blah.discord.api.IDiscordClient
+import sx.blah.discord.api.events.EventDispatcher
+import sx.blah.discord.handle.impl.events.ReadyEvent
 
 /**
  * @author Vidmantas on 2016-10-07.
  */
 class DiscordAuthenticatorTest {
+    private lateinit var clientRetriever: ClientRetriever
+    private lateinit var client: IDiscordClient
+    private lateinit var authenticator: DiscordAuthenticator
+    private lateinit var dispatcher: EventDispatcher
+
+    @Before
+    fun setUp() {
+        clientRetriever = mock(ClientRetriever::class.java)
+        client = mock(IDiscordClient::class.java)
+        `when`(clientRetriever.getClient()).thenReturn(client)
+        dispatcher = mock(EventDispatcher::class.java)
+        `when`(client.dispatcher).thenReturn(dispatcher)
+        authenticator = DiscordAuthenticator(clientRetriever)
+    }
+
     @Test
     fun testLogin_callsClientLogin() {
-        val clientRetriever = mock(ClientRetriever::class.java)
-        val client = mock(IDiscordClient::class.java)
-        `when`(clientRetriever.getClient()).thenReturn(client)
-        val authenticator = DiscordAuthenticator(clientRetriever)
-
         authenticator.login()
 
         verify(client).login()
+    }
+
+    @Test
+    fun testLogin_registersEventDispatcher() {
+        authenticator.login()
+
+        verify(dispatcher).registerListener(authenticator)
+    }
+
+    @Test
+    fun testOnReady_passesWithoutException() {
+        authenticator.onReady(mock(ReadyEvent::class.java))
     }
 }

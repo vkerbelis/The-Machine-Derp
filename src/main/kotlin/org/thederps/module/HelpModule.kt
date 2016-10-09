8e4e7eb510d75d60979db8979bc2c150fac2b6a4
Vidmantas Kerbelis <vkerbelis@yahoo.com>
2016-10-09 13:44:17 +0300

    Refactor appendCommands into an extension function

--- src/main/kotlin/org/thederps/module/HelpModule.kt
+++ src/main/kotlin/org/thederps/module/HelpModule.kt
@@ -2,13 +2,11 @@ package org.thederps.module
 
 import org.thederps.module.receiver.MessageReceiver
 import org.thederps.tools.MessageCreator
-import org.thederps.tools.appendCommand
+import org.thederps.tools.appendCommands
 import org.thederps.tools.commandValid
-import org.thederps.tools.hasExecutableCommand
 import sx.blah.discord.api.IDiscordClient
 import sx.blah.discord.api.events.EventSubscriber
 import sx.blah.discord.handle.impl.events.MessageReceivedEvent
-import sx.blah.discord.util.MessageBuilder
 
 /**
  * @author Vidmantas on 2016-10-09.
@@ -39,23 +37,13 @@ class HelpModule(
     override fun onMessage(event: MessageReceivedEvent) {
         val message = event.message
         if (commandValid(event)) {
-            val builder = messageCreator.with(event.client)
+            messageCreator.with(event.client)
                     .withChannel(message.channel)
                     .withContent("Hi there, I can accept the following commands:\n")
+                    .appendContent("---------------------------\n")
+                    .appendCommands(moduleRetriever)
                     .appendContent("---------------------------")
-            appendCommands(builder)
-            builder.appendContent("---------------------------")
                     .build()
-        }
-    }
-
-    private fun appendCommands(builder: MessageBuilder) {
-        var position = 1
-        moduleRetriever.getModules().forEach { module ->
-            if (module.hasExecutableCommand()) {
-                builder.appendCommand(position, module.command, module.description)
-                position++
-            }
         }
     }
 }
--- src/main/kotlin/org/thederps/tools/CommandExt.kt
+++ src/main/kotlin/org/thederps/tools/CommandExt.kt
@@ -1,6 +1,7 @@
 package org.thederps.tools
 
 import org.thederps.module.Module
+import org.thederps.module.ModuleRetriever
 import sx.blah.discord.handle.impl.events.MessageReceivedEvent
 import sx.blah.discord.util.MessageBuilder
 
@@ -10,6 +11,17 @@ import sx.blah.discord.util.MessageBuilder
 fun Module.commandValid(event: MessageReceivedEvent): Boolean {
     val message = event.message
     return message.content.startsWith(this.command) && !message.author.isBot
+}
+
+fun MessageBuilder.appendCommands(moduleRetriever: ModuleRetriever): MessageBuilder {
+    var position = 1
+    moduleRetriever.getModules().forEach { module ->
+        if (module.hasExecutableCommand()) {
+            appendCommand(position, module.command, module.description)
+            position++
+        }
+    }
+    return this
 }
 
 fun MessageBuilder.appendCommand(position: Int, command: String, description: String) {
